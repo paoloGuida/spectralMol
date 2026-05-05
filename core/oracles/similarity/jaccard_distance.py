12d0674e4589daa9d2cce0e56a126a9299ca099f
barradd <didier.barradasbautista@kaust.edu.sa>
2026-05-05 14:51:30 +0300

    Fix reference_smiles key retrieval in JaccardDistance class and add RAPIDS + Dask acceleration plan documentation , change line 16

--- core/oracles/similarity/jaccard_distance.py
+++ core/oracles/similarity/jaccard_distance.py
@@ -13,7 +13,7 @@ class JaccardDistance(OracleComponent):
         self.radius = self.parameters.specific_parameters.get("radius", 3)
         self.use_counts = self.parameters.specific_parameters.get("use_counts", True)
         self.use_features = self.parameters.specific_parameters.get("use_features", True)
-        self.reference_smiles = self.parameters.specific_parameters.get(self.component_specific_parameters.SMILES, [])
+        self.reference_smiles = self.parameters.specific_parameters.get("smiles", [])
         self.reference_mols = [Chem.MolFromSmiles(smiles) for smiles in self.reference_smiles]
         self.reference_fingerprints = [
             GetMorganFingerprint(mol=mol, radius=self.radius, useCounts=self.use_counts, useFeatures=self.use_features) for mol in self.reference_mols
